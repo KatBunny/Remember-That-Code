940af0afa13dbed50864bd9b7411f6eef449387e
katherinewonders <108916070+katherinesinkler@users.noreply.github.com>
2023-01-05 13:25:41 -0700

    I am commiting some beginng js. I used a W3schools editor to learn about drop menu and it dropped and never came back up so I will check into that soon

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log('here');
+
+
+// open drop menu 
+function myFunction (){
+    document.getElementById("myDropdown").classList.toggle("show");
+};
+
+// close drop menu
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        let dropdown = document.getElementsByClassName("dropdown-content");
+        let i 
+        for (i = 0;i < dropdown.length; i++) {
+            let openDrop = dropdown[i];
+            if (openDrop.classList.contains('show')) {
+                openDrop.classList.remove('show');
+            }
+        }
+    }
+}
