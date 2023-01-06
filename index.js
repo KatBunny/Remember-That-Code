58cb9478f221f9042c9b3434d952aae0eed7c7eb
katherinewonders <108916070+katherinesinkler@users.noreply.github.com>
2023-01-06 08:17:07 -0700

    I am going to just start over on JS; I got into a google and youtube rabbit hole and its just making my vision disappear. I'm also going to edit my html for now.

--- index.js
+++ index.js
@@ -1,21 +1,84 @@
 console.log('here');
 
 
-// open drop menu 
-function myFunction (){
-    document.getElementById("myDropdown").classList.toggle("show");
-};
+// // open drop menu 
+// function myFunction (){
+//     document.getElementById("myDropdown").classList.toggle("show");
+// };
 
-// close drop menu
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        let dropdown = document.getElementsByClassName("dropdown-content");
-        let i 
-        for (i = 0;i < dropdown.length; i++) {
-            let openDrop = dropdown[i];
-            if (openDrop.classList.contains('show')) {
-                openDrop.classList.remove('show');
-            }
-        }
-    }
-}
+// // close drop menu
+// window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//         let dropdown = document.getElementsByClassName("dropdown-content");
+//         let i 
+//         for (i = 0;i < dropdown.length; i++) {
+//             let openDrop = dropdown[i];
+//             if (openDrop.classList.contains('show')) {
+//                 openDrop.classList.remove('show');
+//             }
+//         }
+//     }
+// }
+
+    Create 2 arrays with different, yet matching card values.
+
+        ``` const terms = ["href", "!(bang operator)", "head", "...", "...", ".."];
+            const meanings = [" URL of page link..", "creates html skeleton", "container for data about data", "...", "...", ".."]
+        ```
+         Create a function to generate the arrays.
+
+       ```  function htmlColumns (col1, col2) {
+                for(...)
+                console.log(...)
+                } elseIf {
+                    ...
+                }; 
+
+                let cards = ["a", "b", "c", "d", "e", "f"]
+                let myFunction = ()
+                
+//     ```        
+//         Creat a foor loop to itterate through the arrays.
+
+//         ``` for (let i = 0; ...; ..) {
+//                 for(let ......length);
+//                 const cards = htmlColumns[..];
+//         }
+//         return cards;
+//         ```
+
+//     Create a math randomizer function.
+
+//         ``` function randomC (cards) {
+//             ..
+//         }
+//         randomC (cards);
+//         ```
+//            Create a class Expression of 'Columns' with constructor of 2 paremeters: 
+//         1.) coding terms
+//         2.) basic meanings
+
+//         ``` const Columns = class {
+//             constructor(codingTerms, basicMeanings) {
+//                 this.codingTerms = codingTerms;
+//                 this.width = codingWidth;
+//             }
+//         }; 
+//         ```
+//              Create a drop down menu or a button within the nav bar.
+
+//      ```` < div class = "dropdown">
+//                 <button>...
+//                     <a href = "">Link</a>
+//                 </div>
+//         ```
+//   Create a forloop that iterates the score percentile and print to console the ner result.
+
+//         ``` const starterScore = "100"
+//         function starterSore (score); {
+//         for (let i = 5; i < 100; i -- ); {
+//             console.log( "Your score is " + score)
+//             } 
+//         };
+//         return starterScore(i);
+//         ```
