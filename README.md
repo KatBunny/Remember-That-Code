0d106678d8c80ae2ef2d0322d3c72c11d7acf835
KatBunny <120514198+KatBunny@users.noreply.github.com>
2023-01-17 14:44:24 -0700

    A newer version
    Not fully functional or tidy as it will be, but definitely better than it is now.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-https://katbunny.github.io/Remember-That-Code/
+## https://katbunny.github.io/Remember-That-Code/
+
+image.png
+
+### Hello, welcome to "Remember that code"; this is a game designed to help yo study common terms that are important to remember as you are just starting your web dev journey. 
+
+#### The game is designed to be a memory game designed with flip cards that flip as the cursor hovers over a particular card. When a match is recognized it dissappears. When there are no more matches, then you have completed a succesful study lesson.
+
+#### There aren't many rules, however, there is a beginning of game score set at 100 points and every time you miss a match a 5 point score will be reduced, so be careful not to click unless you mean it!
--- index.js
+++ index.js
@@ -1,148 +1,154 @@
-// const URL = "https://katbunny.github.io/Remember-That-Code/" // nice
-// function 
+console.log ("hola");
 
-const gridflashC = document.querySelector('.grid-flashC')
+ const URL = "https://katbunny.github.io/Remember-That-Code/"
+
+//  fetch("https://katbunny.github.io/Remember-That-Code/"); 
+//  .then(data => id('grid-flashC'))
+//   function list(cardChosen){
+//       .checkForMatch(err =>
+//          console.log(err))
+
+//          .then(data =>(data)) // you could console.log(data) 
+    
+    const cardArr = [ 
+        {
+            name: 'array',
+            img:'images/My project-14.jpg'
+        },
+         {
+            name: 'array',
+            img:'images/My project-15.jpg'
+        },
+         {
+            name: 'const/let',
+            img:'images/My project-22.jpg'
+        },
+         {
+            name: 'const/let',
+            img:'images/My project-22.jpg'
+        },
+         {
+            name: 'api',
+            img:'images/My project-13.jpg'
+        },
+         {
+            name: 'api',
+            img:'images/My project-14.jpg'
+        },
+         {
+            name: 'html',
+            img:''
+        },
+         {
+            name: 'html',
+            img:'images/My project-18.jpg'
+        },
+         {
+            name: 'object',
+            img:'images/My project-21.jpg'
+        },
+         {
+            name: 'object',
+            img:'images/My project-17.jpg'
+        },
+         {
+            name: 'mkdir',
+            img:'images/My project-6.jpg'
+        },
+         {
+            name: 'mkdir',
+            img:'images/My project-7.jpg'
+        },
+         {
+            name: 'loop',
+            img:'images/My project-16.jpg'
+        },
+         {
+            name: 'loop',
+            img:'images/My project-20.jpg'
+        },
+         {
+            name: 'function',
+            img:''
+        },
+         {
+            name: 'function',
+            img:'images/My project-22.jpg'
+        },
+         {
+            name: 'function',
+            img:'images/My project-12.jpg'
+        },
+         {
+            name: 'node',
+            img:'images/My project-2.jpg'
+        },
+          {
+            name: 'node',
+            img:'images/My project-24.jpg'
+        },
+          {
+            name: 'ajax',
+            img:'images/My project-5.jpg'
+        },
+          {
+            name: 'ajax',
+            img:'images/ajaxAns.jpg'
+        },
+    ]
+
+
+    cardArr.sort[(''), 0.5 - Math.random()]
+
+
+const gridflashC = document.querySelector('.grid-flashC'),
+const resultDisplay = document.querySelector('#result'),
+const cardsChosenId = ['']
+const cardsWon = ['']
+
 
 function createBoard() {
-    for (let i = 0; i < cardArray.length; i++){
-    const card = document.createElement('HTMLDivElement',
-    card.setAttribute(('src', 'images/blank.jpeg'),
-    card.setAttribute('flashC', i),
-    //card.addEventListener('click', flipcard)
-    gridflashC.appendChild(card)));
+    for (let i = 0; i < cardArr.length; i++){
+    card = document.createElement('img'),
+    card.setAttribute('src', 'images/blank.jpeg'),
+    card.setAttribute('data-id', i),
+    card.addEventListener('click', flipcard),
+    gridflashC.appendChild(card)
+}
+}
+
+
+//matches
+function checkForMatch() {
+    const cards = document.querySelectorAll('img')
+    const optionOneId = cardChosenId[0]
+    const optionTwoid = cardChosenId[1]
+    if (cardsChosen[0] === cardsChosen[1]) {
+        console.log('Perfet Match!')
+        cards(optionOneId).setAttribute('src', 'images/white')
+        cards(optionTwoId).setAttribute('src', 'images/white')
+        cardsWon.push(cardsChosen)
+    } else {
+        cards(optionOneId).setAttribute('src', 'images/blank.png')
+        cards(optionTwoid).setAttribute('src', 'images/blank.png')
+        console.log('Give it another Go!')
+    }
+    cardChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if(cardsWon.length === cardArr.length/2) {
+        resultDisplay.textContent = ('You Are a JavaScript Guru! Keep it Up!')
     }
 }
 
 
-// fetch("https://katbunny.github.io/Remember-That-Code/"); 
-// .then((data => list(.grid-flashC)))
-//  function list(data){
-//      .try(err => console.log(err))
-//     .then(data =>(data)) // you could console.log(data) 
- 
+function flipcard() {
+    let cardId = this.getAttribute('images/My project-21.jpg')
+    cardChosen.push(cardArr(cardId).name)
+    cardChosenId.push(cardId)
+    this.setAttribute('src', cardArr(cardId).img)
+    if (cardsChosen.length === 2) {
+        setTimeout(checkForMatch, 500)
+    }
+}
 
-
-// const data =   (cardArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]); // incomplete assignment
-
-// // }
-// //     .then(response => response.json()) // this .then() chain needs to be attached to fetch(URL)
-// //     .then(data => list(data)) // 
-// //     .try(err => console.log(err))
-// //     .then(data =>(data)) // you could console.log(data) here to see what you got from the fetch
-// // }
-//  const name = document.getElementById('flashC') // this section is a good start
-// // const image = document.getElementById('cake-image')
-// // const description = document.getElementById("cake-description")
-// const reviews = document.getElementById("flashC").innerHTML += "";
-
-//  function addDivID() {
-//             document.getElementById("flashC").innerHTML +=
-//             <h3>"AJAX"</h3>;
-//         }
-        
-//         // $("div#clickable").click(function(){ window.location = "http://whatever.com/"});
-
-//  document.addEventListener('DOMContentLoaded', () => {
-//      //call on my jsCards here
-//      const cardArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-
-//        cardArr.sort[(''), 0.5 - Math.random()]
-
-// //       //make board for cards to exist
-
-//        const grid = document.querySelector('.grid-flashC')
-//        const resultDisplay = document.querySelector('#result')
-//        const cardsChosenId = []
-//        const cardsWon = []
-
-//        function createBoard() {
-//          for (let i = 0; i < cardArr.length; i++) {
-//              const card =document.createElement('img')
-//              card.setAttribute('src','images/blank.png')
-//              card.setAttribute('data-id', ['i'])
-//              card.addEventListener('click, flipcard')
-//              grid.appendChild(card)
-//              //matches
-
-//              function checkForMatch() {
-//                  const cards = document.querySelectorAll('img')
-//                  const optionOneId = cardChosenId[0]
-//                  const optionTwoid = cardChosenId[1]
-//                  if (cardsChosen[0] === cardsChosen[1]) {
-//                      alert('Perfet Match!')
-//                      cards(optionOneId).setAttribute('src', 'images/white')
-//                      cards(optionTwoId).setAttribute('src', 'images/white')
-//                      cardsWon.push(cardsChosen)
-//                  } else {
-//                      cards(optionOneId).setAttribute('src', 'images/blank.png')
-//                      cards(optionTwoid).setAttribute('src', 'images/blank.png')
-//                      alert('Give it another Go!')
-//                 }
-//                  cardsChosen = []
-//                  cardsChosenId = []
-//                  resultDisplay.textContent = cardsWon.length
-//                  if(cardsWon.length === cardArr.length/2) {
-//                      resultDisplay.textContent = "You Are a JavaScript Guru! Keep it Up!"
-//                  }
-
-//                }
-
-//           }
-
-
-
-//              }
-//              //flip card
-
-//              function flipcard() {
-//                  let cardId = this.getAttribute('data-id')
-//                  cardChosen.push(cardArr(cardId).none)
-//                  cardChosenId.push(cardId)
-//                  this.setAttribute('src', cardArr(cardId).img)
-//                  if (cardsChosen.length === 2, {
-//                      setTimeout(checkForMatch);
-//                   })
-             
-         
-    
-
-// createBoard()
-//                 }
-            
-
-
-// // // open drop menu 
-//  function myFunction (){
-//      document.getElementById("myDropdown").classList.toggle("show");
-//  };
-
-// // // close drop menu
-//  window.onclick = function(event) {
-//      if (!event.target.matches('.dropbtn')) {
-//          let dropdown = document.getElementsByClassName("dropDown-content");
-//          let i 
-//          for (i = 0;i < dropdown.length; i++) {
-//              let openDrop = dropdown[i];
-//              if (openDrop.classList.contains('show')) {
-//                  openDrop.classList.remove('show');
-//              }
-//          }
-//      }
-//  }
-
-
-
-
-//  function startGame() {
-//      myGameArea.start();
-//  }
-
-// let cardsBaseCanvas = {
-//     canvas : document.createElement("canvas"),
-//     start : function() {
-//         this.canvas.width = 480;
-//         this.canvas.height = 270;
-//         this.context = this.canvas.getContext("2d");
-//         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-//     }
-// }
